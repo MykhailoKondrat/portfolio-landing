ef2e86d55454463e9495f7a6cf4ef0f48e2ad526
Mykhailo Kondrat <mykhailo.kondrat@gmail.com>
2020-03-04 14:44:38 +0200

    add scroll, fix animations,add close button, fix paddings on scroll

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
+import $ from 'jquery';
 import 'bootstrap';
 import "./scss/main.scss";
-import $ from 'jquery';
-console.log("hello, world");
 
-$( window ).scroll(function() {
-  console.log('scrolling sxrilling scjllsjdclads');
-})
+//
+$(document).ready(function(){
+	$( window ).scroll(function() {
+
+	//$( ".navbar" ).animate({"margin":"0"},"slow");
+	if ($(window).scrollTop() >= 200) { 
+               $( ".navbar" ).css("box-shadow"," 0px -1px 43px -16px rgba(0,0,0,0.75)"); 
+                
+           }else {
+               $( ".navbar" ).css("box-shadow"," none"); 
+           	
+           }
+	})
+
+	if ($(window).width() <= 767.93) {  
+
+             $(".nav-item").attr({"data-toggle":"collapse","data-target":".navbar-collapse"});
+             $(".navbar-toggler-icon").add(".nav-link").on("click", function(){
+    			 $(".navbar-toggler-icon").toggleClass("active-toggler");
+   			 });
+       }  
+    
+});
